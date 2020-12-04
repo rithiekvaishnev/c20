d2eb2da281dec73670b0638d0abd0ae10bdc993c
rithiekvaishnev <74966777+rithiekvaishnev@users.noreply.github.com>
2020-12-04 10:56:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var car,wall;
+var speed,weight
+
+function setup() {
+  createCanvas(800,400);
+  car = createSprite(80, 200, 50, 50);
+  car.debug = false;
+
+  wall = createSprite(700,200,60,height/2);
+  wall.shapeColour="grey";
+
+  speed=random(20,90);
+  weight=random(500,1500);
+
+  car.velocityX = speed;
+}
+
+function draw() {
+  background("black");
+
+  if(wall.x-car.x<(car.width+wall.width/2)){
+    car.velocityX=0;
+    var deformation = 0.5* weight*speed*speed/22500;
+    if(deformation>180){
+      car.shapeColour=color(255,0,0);
+    }
+    if(deformation<180 && deformation>100){
+      car.shapeColour=color(230,230,0);
+    }
+    if(deformation<100){
+      car.shapeColour=color(0,255,0);
+    }
+
+  }
+
+  drawSprites();
+}
